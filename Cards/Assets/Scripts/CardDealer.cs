ce19d097b654680fd4d8330f235a7b52aedd5c8d
erina0405 <erina.n0125@gmail.com>
2020-07-12 16:13:40 +0900

    ポーカーロジックまで実装

---
+++ Cards/Assets/Scripts/CardDealer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardDealer : MonoBehaviour
+{
+    private List<Card> deck = new List<Card>();
+    private List<Card> playerHand = new List<Card>();
+
+    void Start()
+    {
+        deck = Deck.ShuffleDeck(Deck.GetDeck());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CardDeal();
+        }
+    }
+
+    public void CardDeal()
+    {
+        playerHand.Clear();
+
+        if (deck.Count < 5)
+        {
+            deck.Clear();
+            deck = Deck.ShuffleDeck(Deck.GetDeck());
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            playerHand.Add(Deck.GetCard(deck));
+        }
+
+        foreach (var card in playerHand)
+        {
+            Debug.Log($"{card.CardSuit}:{card.Number}");
+        }
+        Debug.Log(PokerHand.CardHand(playerHand));
+    }
+}
---
+++ Cards/Assets/Scripts/Deck.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Deck 
+{
+    /// <summary>
+    /// 昇順のDeckをゲットする
+    /// </summary>
+    /// <returns></returns>
+    public static List<Card> GetDeck()
+    {
+        var deck =　new List<Card>();
+
+        for (int i = 0; i < Card.CardMax; i ++)
+        {
+            deck.Add(new Card(Card.CardSuitJudge(i), Card.CardNumJudge(i)));
+        }
+        return deck;
+    }
+    
+    /// <summary>
+    /// シャッフルしたDeckをゲットする
+    /// </summary>
+    /// <param name="deck"></param>
+    /// <returns></returns>
+    public static List<Card> ShuffleDeck(List<Card> deck)
+    {
+        var shuffleDeck = deck.OrderBy(Card => Guid.NewGuid()).ToList();
+        return shuffleDeck;
+    }
+     /// <summary>
+     /// カードをDeckの中から取得する
+     /// </summary>
+     /// <param name="deck"></param>
+     /// <returns></returns>
+    public static Card GetCard(List<Card> deck)
+    {
+        var card = deck.First();
+        deck.RemoveAt(0);
+        return card;
+    }
+}
